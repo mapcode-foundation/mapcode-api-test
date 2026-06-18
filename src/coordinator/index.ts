e1cb1ae608ea31a6dd31a3fb3a2071aa1e2b73e8
Rijn Buve <rijn@buve.nl>
2026-06-18 15:03:50 +0200

    fixed

--- src/coordinator/index.ts
+++ src/coordinator/index.ts
@@ -2,7 +2,12 @@ import { createServerApp } from "./server";
 
 const portArgIndex = process.argv.indexOf("--port");
 const noOpen = process.argv.includes("--no-open");
-const port = portArgIndex >= 0 ? Number(process.argv[portArgIndex + 1]) : 4173;
+const port =
+  portArgIndex >= 0
+    ? Number(process.argv[portArgIndex + 1])
+    : process.env.PORT
+      ? Number(process.env.PORT)
+      : 4173;
 const app = createServerApp();
 
 app.listen(port, "127.0.0.1", () => {
